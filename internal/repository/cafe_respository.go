5aeaa2405ec07310810f4ecd39cee6df22a7b5e3
Nuttachai-K <korn.jp.nuttachai@gmail.com>
2026-06-15 22:56:43 +0900

    Create cafe resopsitory struture base

---
+++ internal/repository/cafe_respository.go
@@ -0,0 +1,35 @@
+package repository
+
+import (
+	"database/sql"
+
+	"github.com/Nuttachai-K/cafe-finder-api/internal/model"
+)
+
+type CafeRespository interface {
+	GetByID(id int) (*model.Cafe, error)
+	GetAll() ([]model.Cafe, error)
+	Create(cafe *model.Cafe) error
+}
+
+type cafeRespository struct {
+	db *sql.DB
+}
+
+func NewCafeRespository(db *sql.DB) CafeRespository {
+	return &cafeRespository{
+		db: db,
+	}
+}
+
+func (c cafeRespository) GetByID(id int) (*model.Cafe, error) {
+	return &model.Cafe{}, nil
+}
+
+func (c cafeRespository) GetAll() ([]model.Cafe, error) {
+	return []model.Cafe{}, nil
+}
+
+func (c cafeRespository) Create(cafe *model.Cafe) error {
+	return nil
+}
